8fc78dcd7be2df8a7ee06a684db721d8530680cb
mion00 <mion00@gmail.com>
2015-12-01 17:40:24 +0100

    Commit sprint1

--- js/app.js
+++ js/app.js
@@ -3,24 +3,74 @@
 
 // You can modify this object, add functions that you need
 var MailModel = {
-  /**
-   * Initialises the model with the "database" of filter rules
-   * and messages. This function is already implemented.
-   */
-   init : function(){
-     this.rules = rules;
-     this.messages = msgs;
-   }, 
-  
-   /**
-    * Filters out messages in the "database" that match the spam rules.
-    * @return an array of messages, excluding those that match the filter rules.
-    */
-    filter : function(){
-      return [];
+    /**
+     * Initialises the model with the "database" of filter rules
+     * and messages. This function is already implemented.
+     */
+    init: function () {
+        this.rules = rules;
+        this.messages = msgs;
+    },
+
+    matchFilter: function (message) {
+        var found = false;
+        this.rules.forEach(function(rule) {
+            if ((message.indexOf(rule)) != -1) {
+                //console.log(message.indexOf(rule));
+                found = true;
+            }
+        });
+        return found;
+    },
+    /**
+     * Filters out messages in the "database" that match the spam rules.
+     * @return array of messages, excluding those that match the filter rules.
+     */
+    filter: function () {
+        var messages = [];
+        this.messages.forEach(function (elem) {
+            if (!MailModel.matchFilter(elem)) {
+                //console.log(elem);
+                messages.push(elem);
+            }
+        });
+        return messages;
     }
 
-  
+
+};
+
+var MailController = {
+    filtered: false,
+    init: function() {
+        MailModel.init();
+        MailView.init();
+        MailView.showMail(MailModel.messages);
+        $("#filter").click(MailController.filter);
+    },
+    filter: function(event) {
+        if (MailController.filtered) {
+            MailController.filtered = false;
+            MailView.showMail(MailModel.messages);
+        } else {
+            MailController.filtered = true;
+            MailView.showMail(MailModel.filter());
+        }
+    }
+};
+
+var MailView = {
+    init: function() {
+        MailView.list = $("ul.result");
+    },
+    showMail: function(mails) {
+        //console.log(mails);
+        MailView.list.empty();
+        mails.forEach(function(elem) {
+            var html = "<ul>"+elem+"</ul>";
+            MailView.list.append(html);
+        });
+    }
 };
 
 // Example of usage:
@@ -33,7 +83,6 @@ var MailModel = {
 // you can add here your views and controllers if you decide to do so.
 
 
-
-$(document).ready(function(){
-
+$(document).ready(function () {
+    MailController.init();
 });
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,19 @@
-describe("MailModel", function() {
-  it("should..", function(){
-  });
- 
+describe("MailModel", function () {
+    it("should load the initial data", function () {
+        expect(MailModel.messages).toBeDefined();
+        expect(MailModel.rules).toBeDefined();
+        expect(MailModel.messages).toContain("news@spam.com");
+        expect(MailModel.rules).toContain("spam.com");
+    });
+
+    it("the filter should always return something", function() {
+        expect(MailModel.filter()).toBeDefined();
+    });
+
+});
+
+describe("MailController", function() {
+    it("should be unfiltered initially", function() {
+        expect(MailController.filtered).toBeFalsy();
+    });
 });
