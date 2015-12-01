12d9378dccb01078e71994b5430d4d1dd7b23620
mion00 <mion00@gmail.com>
2015-12-01 18:40:14 +0100

    Aggiunti spec per filtro mail da controller

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -9,11 +9,38 @@ describe("MailModel", function () {
     it("the filter should always return something", function() {
         expect(MailModel.filter()).toBeDefined();
     });
-
+    it("the filter should correctly filter the emails", function() {
+        var mails = MailModel.filter();
+        mails.forEach(function(mail) {
+            MailModel.rules.forEach(function(rule) {
+               expect(mail).not.toContain(rule);
+            });
+        })
+    })
 });
 
 describe("MailController", function() {
     it("should be unfiltered initially", function() {
         expect(MailController.filtered).toBeFalsy();
     });
+    it("should return all mails if not filtered", function() {
+        spyOn(MailView, 'showMail');
+        MailController.filtered = true;
+        MailController.filter();
+        expect(MailView.showMail).toHaveBeenCalled();
+        expect(MailView.showMail.mostRecentCall.args[0]).toBe(MailModel.messages);
+    });
+    it("should return only allowed mails if filtered", function() {
+        spyOn(MailView, 'showMail');
+        MailController.filtered = false;
+        MailController.filter();
+        expect(MailView.showMail).toHaveBeenCalled();
+
+        var filteredMails = MailModel.filter();
+        var viewMails = MailView.showMail.mostRecentCall.args[0];
+        expect(filteredMails.length).toBe(viewMails.length);
+        viewMails.forEach(function(elem) {
+            expect(filteredMails).toContain(elem);
+        });
+    })
 });
